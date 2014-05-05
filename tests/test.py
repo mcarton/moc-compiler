71ad08322c82aefb7065e2370f602f25d8dfd99a
viod <maxence.ahlouche@gmail.com>
2014-05-05 14:55:22 +0200

    Refactor tests script

--- tests/test.py
+++ tests/test.py
@@ -14,6 +14,22 @@ NORMAL = '\033[0;00m'
 MOCC_PATH = '..'
 MOCC = 'mocc'
 
+success_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk('success')
+                 for f in filenames if os.path.splitext(f)[1] == '.moc']
+warning_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk('warning')
+                 for f in filenames if os.path.splitext(f)[1] == '.moc']
+failure_files =  [os.path.join(dp, f) for dp, dn, filenames in os.walk('failure')
+                  for f in filenames if os.path.splitext(f)[1] == '.moc']
+
+# associate each return code to the correct semantic
+RETURN_CODES = {
+    0: 'success',
+    1: 'warning',
+    2: 'error',
+}
+# and vice versa
+RETURN_CODES.update({v:k for k, v in RETURN_CODES.items()})
+
 def success(message):
     return GREEN + message + NORMAL
 
@@ -38,56 +54,38 @@ def run_test(f):
 
     return return_code
 
-success_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk('success')
-                 for f in filenames if os.path.splitext(f)[1] == '.moc']
-warning_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk('warning')
-                 for f in filenames if os.path.splitext(f)[1] == '.moc']
-failure_files =  [os.path.join(dp, f) for dp, dn, filenames in os.walk('failure')
-                  for f in filenames if os.path.splitext(f)[1] == '.moc']
+def run_tests(files, expected_return_code):
+    nb_fail = 0
+    nb_success = 0
 
-nb_fail = 0
-nb_success = 0
+    for f in files:
+        return_code = run_test(f)
 
-for f in success_files:
-    return_code = run_test(f)
+        if return_code == expected_return_code:
+            nb_success += 1
+            print(success('OK'))
+        else:
+            nb_fail += 1
+            print(error('FAIL'))
+            print(error('    Expected success, found {}.'.format(RETURN_CODES[return_code])))
 
-    if return_code == 0:
-        nb_success += 1
-        print(success('OK'))
+    return nb_fail, nb_success
+
+if __name__ == '__main__':
+    nb_fail = 0
+    nb_success = 0
+
+    a, b = run_tests(success_files, RETURN_CODES['success'])
+    nb_fail, nb_success = nb_fail + a, nb_success + b
+    a, b = run_tests(warning_files, RETURN_CODES['warning'])
+    nb_fail, nb_success = nb_fail + a, nb_success + b
+    a, b = run_tests(failure_files, RETURN_CODES['error'])
+    nb_fail, nb_success = nb_fail + a, nb_success + b
+
+    print('\nSummary\n-------')
+    if nb_fail == 0:
+        print(success('Everything is OK.'))
     else:
-        nb_fail += 1
-        print(error('FAIL'))
-        print(error('    Expected success, found {}.'.format(
-            'warning' if return_code == 1 else 'error')))
-
-for f in warning_files:
-    return_code = run_test(f)
-
-    if return_code == 1:
-        nb_success += 1
-        print(success('OK'))
-    else:
-        nb_fail += 1
-        print(error('FAIL'))
-        print(error('    Expected warning, found {}.'.format(
-            'success' if return_code == 0 else 'error')))
-
-for f in failure_files:
-    return_code = run_test(f)
-
-    if return_code == 2:
-        nb_success += 1
-        print(success('OK'))
-    else:
-        nb_fail += 1
-        print(error('FAIL'))
-        print(error('    Expected error, found {}.'.format(
-            'success' if return_code == 0 else 'warning')))
-
-print('\nSummary\n-------')
-if nb_fail == 0:
-    print(success('Everything is OK.'))
-else:
-    print(success('{} tests passed successfully.'.format(nb_success)))
-    print(error('{} tests failed.'.format(nb_fail)))
-    sys.exit(1)
+        print(success('{} tests passed successfully.'.format(nb_success)))
+        print(error('{} tests failed.'.format(nb_fail)))
+        sys.exit(1)
