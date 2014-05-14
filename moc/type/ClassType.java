e84c531c79a9de165f449e15e14820e281b3331c
mcarton <cartonmartin+git@gmail.com>
2014-05-14 23:41:17 +0200

    Add inheritance

--- moc/type/ClassType.java
+++ moc/type/ClassType.java
@@ -1,14 +1,20 @@
 package moc.type;
 
 public final class ClassType extends AbstractType<ClassType> {
+    ClassType superClass;
     String name;
 
-    public ClassType(String name) {
+    public ClassType(String name, ClassType superClass) {
         this.name = name;
+        this.superClass = superClass;
     }
 
     public String toString() {
         return name;
+    }
+
+    public ClassType getSuper() {
+        return superClass;
     }
 
     @Override
