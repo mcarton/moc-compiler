cbb386cbeec3bd4c36fb834de8d8b71b10e7b22d
mcarton <cartonmartin+git@gmail.com>
2014-05-15 00:34:45 +0200

    Add attributes

---
+++ moc/type/Attributes.java
@@ -0,0 +1,12 @@
+package moc.type;
+
+public final class Attributes {
+    public Type type;
+    public String name;
+
+    public Attributes(Type type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+}
+
--- moc/type/ClassType.java
+++ moc/type/ClassType.java
@@ -1,8 +1,11 @@
 package moc.type;
 
+import java.util.Vector;
+
 public final class ClassType extends AbstractType<ClassType> {
     ClassType superClass;
     String name;
+    Vector<Attributes> attributes = new Vector<>();
 
     public ClassType(String name, ClassType superClass) {
         this.name = name;
@@ -15,6 +18,24 @@ public final class ClassType extends AbstractType<ClassType> {
 
     public ClassType getSuper() {
         return superClass;
+    }
+
+    public void addAttribute(Type type, String name) {
+        attributes.add(new Attributes(type, name));
+    }
+
+    public Type getAttributeType(String name) {
+        for (Attributes att : attributes) {
+            if (att.name.equals(name)) {
+                return att.type;
+            }
+        }
+
+        if (superClass != null) {
+            return superClass.getAttributeType(name);
+        }
+
+        return null;
     }
 
     public boolean inheritsFrom(Type other) {
