fff3ae761d6496ed60fe65aa8d0e11aeeed10292
mcarton <cartonmartin+git@gmail.com>
2014-05-20 22:09:27 +0200

    Add owning class in methods

--- moc/type/Method.java
+++ moc/type/Method.java
@@ -3,14 +3,23 @@ package moc.type;
 import java.util.Vector;
 
 public class Method {
+    ClassType classType;
     Type returnType;
     Vector<Selector> selectors = new Vector<>();
     boolean isStatic;
 
-    public Method(Type returnType, Vector<Selector> selectors, boolean isStatic) {
+    public Method(
+        ClassType classType, Type returnType,
+        Vector<Selector> selectors, boolean isStatic
+    ) {
+        this.classType = classType;
         this.returnType = returnType;
         this.selectors = selectors;
         this.isStatic = isStatic;
+    }
+
+    public ClassType getClassType() {
+        return classType;
     }
 
     public Type getReturnType() {
